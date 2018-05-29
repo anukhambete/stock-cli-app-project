113c0998314186dbed03b60d79b59a4854e9e9b2
Anu Khambete <anukhambete@gmail.com>
2018-05-29 18:20:15 +0000

    Initial commit

--- lib/stock-info/cli.rb
+++ lib/stock-info/cli.rb
@@ -1,7 +1,21 @@
 class StockInfo::CLI
 
+# The cli should display 2 options List and quit and accept a valid input
+# if list then display the list of stocks and symbols
+
   def call
-    puts "Hello World"
+    puts "To view a list of stocks enter list or to quit enter 'quit'"
+    input_1 = gets.strip
+    if["list","quit"].include? input_1.downcase
+      if input_1.downcase == "list"
+        # should display objects which are instances of stocks with name and symbols
+        puts " stock and symbols"
+      elsif input_1.downcase == "quit"
+        puts "You have chosen to exit the app"
+      end
+    else
+      puts "enter a valid option"
+    end
   end
 
 end
